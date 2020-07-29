d316a5e61fb706dc67bcf64d70730c1e90d89aa7
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-07-29 17:28:59 -0500

    feat: Add navigator_header layout html

--- client/main.py
+++ client/main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-mainRoute = Blueprint('main',__name__,template_folder='pages')
+mainRoute = Blueprint('main',__name__,template_folder='pages',static_url_path='',static_folder='static')
 
 @mainRoute.route('/')
 def home():
