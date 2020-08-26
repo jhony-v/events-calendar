8238e6bfa380aa3d79404e9c5591d735072ccd1b
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-08-25 19:07:16 -0500

    feat: Create method to delete a file from bucket

--- client/calendar-events/src/infraestructure/repositories/UploadFilesRepository.ts
+++ client/calendar-events/src/infraestructure/repositories/UploadFilesRepository.ts
@@ -6,10 +6,15 @@ interface UploadReference {
 }
 
 export default class UploadFilesRepository {
-    uploadNewFile = async (dataUpload: UploadReference): Promise<firebase.storage.UploadTaskSnapshot> => {
-        const refStorage: firebase.storage.Reference = storage.ref(dataUpload.path);
-        const putUpload = await refStorage.put(dataUpload.file);
-        return putUpload;
-    };
-}
+  uploadNewFile = async ( dataUpload: UploadReference ): Promise<firebase.storage.UploadTaskSnapshot> => {
+    const refStorage: firebase.storage.Reference = storage.ref(dataUpload.path);
+    const putUpload = await refStorage.put(dataUpload.file);
+    return putUpload;
+  };
 
+  deleteFile = async (path: string): Promise<boolean> => {
+    const refStorage = storage.ref(path);
+    const promiseDelete = refStorage.delete();
+    return promiseDelete.then(() => true).catch(() => false);
+  };
+}
